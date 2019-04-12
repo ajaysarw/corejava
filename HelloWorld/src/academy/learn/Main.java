c2b337bf4b8b6053f13764b00ae5232cb315da26
ajaysarw <ajaysarwai.in@gmail.com>
2019-04-12 18:10:12 +0530

    Init - Added first Hello World Program

---
+++ HelloWorld/src/academy/learn/Main.java
@@ -0,0 +1,12 @@
+package academy.learn;
+
+// class declaration
+public class Main {
+
+    // part of class Main array of params args
+    public static void main(String[] args) {
+	    // write your code here
+        //
+        System.out.println("Hello World");
+    }
+}
